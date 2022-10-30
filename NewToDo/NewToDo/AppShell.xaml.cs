630f1b833960383875b74622688e7b76f55a5aa0
Kal <info@kalacademy.org>
2022-10-30 12:28:41 -0700

    Initial commit

---
+++ NewToDo/NewToDo/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using NewToDo.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NewToDo
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ NewToDo/NewToDo/Views/ToDoPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+namespace NewToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDoPage : ContentPage
+    {
+        public ToDoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                $"{Path.GetRandomFileName()}.notes.txt");
+            File.WriteAllText(filename, ToDoText.Text);
+
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
